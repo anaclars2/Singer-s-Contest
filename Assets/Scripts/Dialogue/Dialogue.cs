dfe54e4ac2445b8232e245eb52a917cf2785d5c8
Amanda Nobre <ans-08@hotmail.com>
2025-04-20 16:39:44 -0300

    Dialogo Pronto
    O dialogo está pronto, porém um polimento seria interessante

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI speakerNameText;
-    public Image speakerIconImage;
+    public Image portraitLeft;
+    public Image portraitRight;
     public TextMeshProUGUI textComponent;
     public List<DialogueLine> lines = new List<DialogueLine>();
     public float textSpeed;
@@ -47,13 +48,22 @@ public class Dialogue : MonoBehaviour
         DialogueLine line = lines[index];
 
         speakerNameText.text = line.speakerName;
-
-        if (line.speakerIcon != null)
-        {
-            speakerIconImage.sprite = line.speakerIcon;
-        }
-
         textComponent.text = "";
+
+        switch(line.side)
+        {
+            case DialogueSide.Left:
+            portraitLeft.gameObject.SetActive(true);
+            portraitLeft.sprite = line.speakerIcon;
+            portraitRight.gameObject.SetActive(false);
+            break;
+
+            case DialogueSide.Right:
+            portraitRight.gameObject.SetActive(true);
+            portraitRight.sprite =line.speakerIcon;
+            portraitLeft.gameObject.SetActive(false);
+            break;
+        }
         
         foreach (char c in line.text.ToCharArray())
         {
@@ -80,7 +90,14 @@ public class Dialogue : MonoBehaviour
 [System.Serializable]
 public class DialogueLine
 {
+    public DialogueSide side;
     public string speakerName;
     public Sprite speakerIcon;
     [TextArea(3, 10)] public string text;
+}
+
+public enum DialogueSide
+{
+    Left,
+    Right
 }
